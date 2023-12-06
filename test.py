e813473e03259dd450f3b3bb3812f4c8750bf007
Ankitkr1375 <148523859+Ankitkr1375@users.noreply.github.com>
2023-12-07 01:34:17 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,52 @@
+a = int(input())
+def prime(a):
+    i = 2
+    count = 0
+    while(i < 2):
+        if a%i==0:
+            count = count + 1
+i = 2
+count = 0
+while
+
+
+import math
+a = 10.57
+math.ceil(a)
+
+math.floor(a)
+
+math.sqrt(5678)
+
+math.factorial(4)
+
+math.gcd(9,27)
+
+math.lcm(6,6)
+
+from math import sqrt
+sqrt(225)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
